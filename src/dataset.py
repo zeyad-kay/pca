eaaed0e71b44ae03e3746e90c179e674dc7d30f0
zeyad <zeyad_khaled@outlook.com>
2022-05-21 12:09:43 +0200

    feat: add labels

--- src/dataset.py
+++ src/dataset.py
@@ -7,20 +7,25 @@ class Dataset:
     from a directory as a numpy array and flattening them for easy use
     in fitting models
     """
-    def __init__(self, dir: str) -> None:
+    def __init__(self, dir: str, ) -> None:
         self.dir = dir
+        self.images = None
+        self.labels = None
 
-    def load(self) -> np.ndarray:
-        """Load the dataset as a numpy array
+    def load(self) -> tuple[np.ndarray,np.ndarray]:
+        """Load images and labels as numpy arrays
 
         Returns:
-            np.ndarray: dataset
+            tuple[np.ndarray,np.ndarray]: images and labels
         """
         dataset = []
+        labels = []
         files = os.listdir(self.dir)
         for f in files:
             dataset.append(self.__load_image(self.dir, f))
-        return np.array(dataset)
+            labels.append(f.split(".")[1])
+        self.images, self.labels = np.array(dataset), np.array(labels)
+        return self.images, self.labels
 
     def __load_image(self, dir, f) -> np.ndarray:
         img = cv2.VideoCapture(os.path.join(dir,f)).read(0)[1]
@@ -39,7 +44,25 @@ class Dataset:
         cv2.imshow(winname,reshaped)
         cv2.waitKey(0)
 
+    def encode_labels(self) -> np.ndarray:
+        """Encode labels to ints
+
+        Returns:
+            np.ndarray: int labels
+        """
+        if self.labels is None:
+            raise ValueError("Load dataset first.")
+
+        encoded = []
+        unique = {}
+        for i in range(self.labels.shape[0]):
+            if not unique.get(self.labels[i]):
+                unique[self.labels[i]] = i
+            encoded.append(unique[self.labels[i]])
+        return np.array(encoded)
+
 if __name__ == "__main__":
     dataset = Dataset("./images/")
-    imgs = dataset.load()
+    imgs,lbls = dataset.load()
+    encoded_lbls = dataset.encode_labels()
     dataset.show_image("test",imgs[0])
